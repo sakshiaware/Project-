4f5fe513d38a8665e92e1898e49ba29be7e94752
gayatrig21 <gayatrigade248@gmail.com>
2022-11-19 14:21:33 +0530

    user module added

---
+++ update backup/SMCSystem/app/src/main/java/com/smc/smcsystem/MainActivity.java
@@ -0,0 +1,30 @@
+package com.smc.smcsystem;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnuser;
+    //SharedPreferences sharedPreferences;
+    // private static final String Shared_pref_name="mypref";
+    // private static final String Key_name="name";
+    // private static final String Key_email="email";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnuser = findViewById(R.id.front_btn1);
+        btnuser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, UserLogin.class));
+            }
+        });
+    }
+}
